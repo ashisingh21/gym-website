14aad556ed44b2b1942800fbb5b93473c8eb14c2
Ashi <ashi888032@gmail.com>
2022-12-13 22:21:15 +0530

    navbar changes 1

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function () { myFunction() };
+window.onscroll = function () { onMyScroll() };
 
-function myFunction() {
+function onMyScroll() {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var scrolled = (winScroll / height) * 100;
     document.getElementById("myBar").style.width = scrolled + "%";
 }
 
-var icon = document.getElementsByClassName("icon");
+// var icon = document.getElementsByClassName("icon");
 
-icon.addEventListener("click", navbarResponsive = () => {
+// icon.addEventListener("click", navbarResponsive = () => {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "topnav";
+//     }
+// }
+// )
+
+function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
@@ -18,5 +28,4 @@ icon.addEventListener("click", navbarResponsive = () => {
         x.className = "topnav";
     }
 }
-)
 
