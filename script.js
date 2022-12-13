3a97f7d288d1ffc491095609604640bff15d036e
Ashi <ashi888032@gmail.com>
2022-12-13 23:01:45 +0530

    navbar changes

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
+
     } else {
         x.className = "topnav";
     }
